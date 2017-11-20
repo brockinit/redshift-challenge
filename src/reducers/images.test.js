c307c140381a7c9a167930e4e2b89e11c948f6a0
brockinit <brocklanoza@gmail.com>
2017-11-20 08:14:51 -1000

    Add tests for actions and async actions

--- src/reducers/images.test.js
+++ src/reducers/images.test.js
@@ -1,10 +1,18 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
 import reducer, {
   GET_IMAGES,
   GET_IMAGES_SUCCESS,
   INCREMENT_PAGE,
   getImages,
   incrementPage,
+  fetchImages,
 } from './images';
+
+const { INFERNO_APP_CLIENT_ID, INFERNO_APP_IMGUR_API_ENDPOINT } = process.env;
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('actions', () => {
   it('getImages should return the correct action', () => {
@@ -18,5 +26,29 @@ describe('actions', () => {
       type: INCREMENT_PAGE,
     };
     expect(incrementPage()).toEqual(expectedAction);
+  });
+});
+
+describe('async actions', () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  test('fetchImages calls GET_IMAGES_SUCCESS', () => {
+    const mockImages = [{ link: 'https://i.somelink.com' }];
+    fetchMock.getOnce('*', {
+      data: mockImages,
+    });
+    const expectedActions = [
+      { type: GET_IMAGES },
+      { type: GET_IMAGES_SUCCESS, images: mockImages },
+    ];
+
+    const store = mockStore({ images: { items: [], currentPage: 0 } });
+
+    return store.dispatch(fetchImages()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
 });
